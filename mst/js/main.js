f84615a64a4ff549664ac6f2f540c21aeafeb968
Egy Maulana <egymaulana777@gmail.com>
2020-11-08 21:23:39 +0700

    Initial commit
    Initial commit

---
+++ mst/js/main.js
@@ -0,0 +1,24 @@
+(function() {
+    var svg = d3.select("svg");
+    
+    function getRandom(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+    
+    function drawCircle(x, y, size) {
+        console.log('Drawing circle at', x, y, size);
+        svg.append("circle")
+            .attr('class', 'click-circle')
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("r", size);
+    }
+    
+    svg.on('click', function() {
+        var coords = d3.mouse(this);
+        console.log(coords);
+        drawCircle(coords[0], coords[1], getRandom(5,50));
+    });
+    
+    
+})();
