c57a18a092832bcbb048cf1cf9f3241aa8eac0c9
Egy Maulana <egymaulana777@gmail.com>
2020-11-11 01:50:00 +0700

    a

---
+++ TruckSystem/js/main.js
@@ -0,0 +1,26 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+  
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+var a = false;
+
+function nav() {
+    if (a == false) {
+        document.getElementById("sidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        a = true;
+    }
+    else if (a == true) {
+        document.getElementById("sidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        a = false;
+    }
+}
